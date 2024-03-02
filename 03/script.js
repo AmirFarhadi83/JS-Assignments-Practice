d35150754a5e06d0cc795e9d919beedea98522a5
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-03-03 00:25:57 +0330

    Update script.js

--- 03/script.js
+++ 03/script.js
@@ -1,13 +1,19 @@
 `use strict`
 
-function describeCountry(country,population,capitalCity) {
-    return `The country ${country} has a population of ${population} million people and its capital city is ${capitalCity}.`;
-}
-const data1=describeCountry("USA",325,"Washington D.C.")
-console.log(data1);
+// // T1 //
 
-const data2=describeCountry('iran',85,'tehran')
-console.log(data2);
+// function describeCountry(country,population,capitalCity) {
+//     return `The country ${country} has a population of ${population} million people and its capital city is ${capitalCity}.`;
+// }
+// const data1=describeCountry("USA",325,"Washington D.C.")
+// console.log(data1);
 
-const data3=describeCountry('russia',130,'moscow')
-console.log(data3);
+// const data2=describeCountry('iran',85,'tehran')
+// console.log(data2);
+
+// const data3=describeCountry('russia',130,'moscow')
+// console.log(data3);
+
+// ///////
+
+
