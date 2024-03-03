87575da448818899dc7e55e2a0ce6cf0939344aa
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-03-03 18:07:19 +0330

    Update script.js

--- 03/script.js
+++ 03/script.js
@@ -17,7 +17,7 @@
 // ///////
 
 
-// // T1 //
+// // T2 //
 
 // function percentageOfWorld1(worldPopulation, countryPopulation){
 //     return (countryPopulation / worldPopulation ) * 100;
@@ -35,4 +35,35 @@
 // console.log(percent3)
 
 // /////////
+
+
+
+// // T3 //
+
+// function percentageOfWorld1(worldPopulation, countryPopulation) {
+//     return (countryPopulation / worldPopulation) * 100;
+// }
+
+// const percentageOfWorld2 = function (worldPopulation, countryPopulation) {
+//     return (countryPopulation / worldPopulation) * 100;
+// }
+
+// const percentageOfWorld3 = (worldPopulation, countryPopulation) => {
+//     return (countryPopulation / worldPopulation) * 100;
+// }
+
+// const percent1 = percentageOfWorld3(7900, 1441)
+// console.log(percent1)
+// const percent2 = percentageOfWorld3(7900, 144)
+// console.log(percent2)
+// const percent3 = percentageOfWorld3(7900, 14)
+// console.log(percent3)
+
+// /////////
+
+
+
+
+
+
 
