896caea3bca9eded13b91cd111717db60b446f84
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-03-05 20:27:18 +0330

    Update script.js

--- 03/script.js
+++ 03/script.js
@@ -303,3 +303,15 @@
 // console.log(`${mark.calcBMI() > john.calcBMI() ? `${mark.fullName}'s BMI (${mark.BMI}) is higher than ${john.fullName} (${john.BMI})` : `${john.fullName}'s BMI (${john.BMI}) is higher than ${mark.fullName} (${mark.BMI})`}`)
 
 // //////////////
+
+
+
+// //// T10 ////
+
+// for (i=1;i<=50;i++){
+//     console.log(`Voter number ${i} is currently voting`)
+// }
+
+// ////////
+
+
