6af5ef5df71e1ff4cf57d66dddd4cf2935db05bb
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-03-06 21:52:53 +0330

    Update script.js

--- 05/script.js
+++ 05/script.js
@@ -1,3 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const x = 23;
+
+const y = 40;
+// This function will return the sum of x and y.
+function add() {
+    "use strict";
+    let z = x + y;
+    console.log(z);
+}
+
+add();  
