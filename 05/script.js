d6775654424e5a19b41a46ee3a9f1e3ea4d7b4f7
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-03-10 20:07:12 +0330

    Update script.js

--- 05/script.js
+++ 05/script.js
@@ -1,2 +1,13 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+'use strict'
+
+const printForecast = function (arr) {
+    let sen = `... `
+    for (let i = 0; i < arr.length; i++) {
+        sen += `${arr[i]}C in ${i + 1} days ... `;
+    }
+    return sen;
+}
+
+const data = printForecast([12,5,-5,0,4]);
+console.log(data)
