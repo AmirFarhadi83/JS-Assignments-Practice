4163b433460d05849b60625de92797d3b44231ad
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-03-13 23:06:40 +0330

    Update script.js

--- 07/05-Guess-My-Number/script.js
+++ 07/05-Guess-My-Number/script.js
@@ -11,13 +11,13 @@
 // console.log(document.querySelector('.guess').value = 2)
 
 
-document.querySelector('.check').addEventListener(
-    'click', () => {
-        const guess = Number(document.querySelector('.guess').value);
-        console.log(guess)
+// document.querySelector('.check').addEventListener(
+//     'click', () => {
+//         const guess = Number(document.querySelector('.guess').value);
+//         console.log(guess)
 
-        if (!guess) {
-            document.querySelector('.message').textContent='⛔ No Number'
-        }
-    }
-)
+//         if (!guess) {
+//             document.querySelector('.message').textContent='⛔ No Number'
+//         }
+//     }
+// )
