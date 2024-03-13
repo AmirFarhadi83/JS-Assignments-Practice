66362e4a4a30be79105f59b5bc7e4f73aeb3d449
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-03-13 22:55:29 +0330

    Update script.js

--- 07/05-Guess-My-Number/script.js
+++ 07/05-Guess-My-Number/script.js
@@ -1,11 +1,18 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
 
-document.querySelector('.number').textContent = 14;
-document.querySelector('.score').textContent = 8;
+// document.querySelector('.number').textContent = 14;
+// document.querySelector('.score').textContent = 8;
 
-document.querySelector('.guess').value = 2;
+// document.querySelector('.guess').value = 2;
 
-console.log(document.querySelector('.guess').value = 2)
+// console.log(document.querySelector('.guess').value = 2)
+
+
+document.querySelector('.check').addEventListener(
+    'click', () => {
+        console.log(document.querySelector('.guess').value)
+    }
+)
