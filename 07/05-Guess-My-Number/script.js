c201e0f55319b976576c82abc8d53bf87ba31ebc
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-03-13 22:41:02 +0330

    Update script.js

--- 07/05-Guess-My-Number/script.js
+++ 07/05-Guess-My-Number/script.js
@@ -2,3 +2,10 @@
 
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 14;
+document.querySelector('.score').textContent = 8;
+
+document.querySelector('.guess').value = 2;
+
+console.log(document.querySelector('.guess').value = 2)
