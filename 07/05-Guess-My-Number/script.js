1a1facf2c88d1275516b355333b249db66e9bf0b
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-03-14 22:27:27 +0330

    Update script.js

--- 07/05-Guess-My-Number/script.js
+++ 07/05-Guess-My-Number/script.js
@@ -1,23 +1,23 @@
 'use strict';
 
 // console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = '🎉 Correct Number!';
 
-// document.querySelector('.number').textContent = 14;
 // document.querySelector('.score').textContent = 8;
 
 // document.querySelector('.guess').value = 2;
 
 // console.log(document.querySelector('.guess').value = 2)
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
-// document.querySelector('.check').addEventListener(
-//     'click', () => {
-//         const guess = Number(document.querySelector('.guess').value);
-//         console.log(guess)
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
 
-//         if (!guess) {
-//             document.querySelector('.message').textContent='⛔ No Number'
-//         }
-//     }
-// )
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  }
+});
