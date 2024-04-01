3ece7101e3ff66827689b35e5f866903a17b6d28
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-04-01 17:48:56 +0330

    Update script.js

--- 07/06-Modal/script.js
+++ 07/06-Modal/script.js
@@ -1,12 +1,29 @@
 'use strict';
 
+/////////////////////////////
+
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
+////////////////////////////
+
+const openModal = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+///////////////////////////
+
 for (let i = 0; i < btnOpenModal.length; i++) {
-    btnOpenModal[i].addEventListener('click', () => {
-      console.log('clicked')
-  });
+  btnOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
