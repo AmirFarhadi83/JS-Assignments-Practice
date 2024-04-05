4680433d3746101ee8704781b2c366708d4f99e3
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-04-05 16:10:28 +0330

    Update script.js

--- 08/script.js
+++ 08/script.js
@@ -6,11 +6,18 @@ function calcAge(birthYear) {
     function printAge() {
         const output = `${firstName}, you are ${age}, born in ${birthYear}`;
         console.log(output);
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            var millennial = true;
+            const str = `Oh, you're millennial, ${firstName}`;
+            console.log(str);
+        }
+        console.log(millennial);
     }
     
     return printAge();
 }
 
 const firstName = 'Amir';
-calcAge(2004);
+calcAge(1988);
   
