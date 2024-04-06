6aa982bd017a54daf24024d66099a06b7b46318a
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-04-06 22:30:14 +0330

    Update script.js

--- 09/script.js
+++ 09/script.js
@@ -50,6 +50,39 @@ const restaurant = {
 };
 
 //////////////////////////////////////////////////
+// Logical Assignments Operators
+//////////////////////////////////////////////////
+
+const rest1 = {
+  name: 'Capri',
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  ownerL: 'Gioanni Rossi',
+};
+
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// Nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
+//////////////////////////////////////////////////
 // Nullish Operators
 //////////////////////////////////////////////////
 
@@ -91,7 +124,6 @@ const restaurant = {
 // };
 
 // restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'cheese');
-
 
 //////////////////////////////////////////////////
 // Rest Operator
