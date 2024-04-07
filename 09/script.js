f7c6a8f064bbf09bc16466107ef225d5d875fd0c
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-04-07 17:09:28 +0330

    Update script.js

--- 09/script.js
+++ 09/script.js
@@ -53,18 +53,18 @@ const restaurant = {
 // Looping Arrays
 //////////////////////////////////////////////////
 
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
-// for (const item of menu) console.log(item);
+// // for (const item of menu) console.log(item);
 
-// for (const item of menu.entries()) {
-//   console.log(`${item[0] + 1}: ${item[1]}`);
-// };
-for (const [i, el] of menu.entries()) {
-  console.log(`${i + 1}: ${el}`);
-}
+// // for (const item of menu.entries()) {
+// //   console.log(`${item[0] + 1}: ${item[1]}`);
+// // };
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el}`);
+// }
 
-// console.log([...menu.entries()]);
+// // console.log([...menu.entries()]);
 
 //////////////////////////////////////////////////
 // Coding Challenge #1
