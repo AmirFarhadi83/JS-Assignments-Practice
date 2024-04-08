3a6453d7d7a43c8081e2cb725448f41506329497
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-04-08 21:16:41 +0330

    Update script.js

--- 09/script.js
+++ 09/script.js
@@ -49,6 +49,41 @@ const restaurant = {
 };
 
 //////////////////////////////////////////////////
+// Maps
+//////////////////////////////////////////////////
+
+const rest = new Map();
+rest.set('name', 'Amir');
+rest.set(1, 'Iran');
+console.log(rest.set(2, 'Tehran'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'we are open :D')
+  .set(false, 'we are closed');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+//////////////////////////////////////////////////
 // Sets
 //////////////////////////////////////////////////
 
@@ -83,7 +118,6 @@ const restaurant = {
 // console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size);
 
 // console.log(new Set('AmirFarhadi').size);
-
 
 //////////////////////////////////////////////////
 // Coding Challenge #2
