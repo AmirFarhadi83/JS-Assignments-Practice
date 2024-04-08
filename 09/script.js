94b09d298000a34ada20c6bf32f394ea8e3566a0
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-04-08 13:18:02 +0330

    Update script.js

--- 09/script.js
+++ 09/script.js
@@ -17,15 +17,15 @@ const openingHours = {
     open: 11,
     close: 23,
   },
-  [`day-${2 + 4}`]: {
+  [weekdays[5]]: {
     open: 0, // Open 24 hours
-    close: 24,
+    close: 12 + 12,
   },
 };
 
 const restaurant = {
   name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  location: 'Via Ang elo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
@@ -47,6 +47,40 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
+//////////////////////////////////////////////////
+// Optional Chaining
+//////////////////////////////////////////////////
+
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
+};
+
+// console.log(restaurant.openingHours.mon.open);
+
+// With Optional Chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`on ${day}, we open at ${open}`);
+};
+
+// Methods 
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [{ name: 'Amir', email: 'amir@gmail.com' }];
+// const users = [];
+
+console.log(users[0]?.name ?? 'user array empty');
+
+if (users.length>0) console.log(users[0].name); else console.log('user array empty');
 
 //////////////////////////////////////////////////
 // Enhanced Object Literals
