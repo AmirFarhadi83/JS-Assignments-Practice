35bee7085cd8a0e455b67b724abd04d1b582f100
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-04-08 20:45:59 +0330

    Update script.js

--- 09/script.js
+++ 09/script.js
@@ -49,6 +49,43 @@ const restaurant = {
 };
 
 //////////////////////////////////////////////////
+// Sets
+//////////////////////////////////////////////////
+
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+console.log(ordersSet);
+
+console.log(new Set('Amir'));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add("Garlic Bread");
+ordersSet.add("Garlic Bread");
+ordersSet.delete('Risotto');
+// ordersSet.clear();
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+// const staffUnique = new Set(staff);
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size);
+
+console.log(new Set('AmirFarhadi').size);
+
+
+//////////////////////////////////////////////////
 // Coding Challenge #2
 //////////////////////////////////////////////////
 
