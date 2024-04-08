0eb82d6d081012aa6132606efa30af18244ea792
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-04-08 14:32:00 +0330

    Update script.js

--- 09/script.js
+++ 09/script.js
@@ -49,6 +49,33 @@ const restaurant = {
 };
 
 //////////////////////////////////////////////////
+// Looping Objects Keys, Values and Entries
+//////////////////////////////////////////////////
+
+// Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `we are open on ${properties.length} days: `;
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// [key, value]
+for (const [day, { open, close }] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
+
+//////////////////////////////////////////////////
 // Optional Chaining
 //////////////////////////////////////////////////
 
@@ -70,7 +97,7 @@ const restaurant = {
 //   console.log(`on ${day}, we open at ${open}`);
 // };
 
-// // Methods 
+// // Methods
 // console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
 // console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
 
