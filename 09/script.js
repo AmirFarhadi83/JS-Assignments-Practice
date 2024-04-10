7617ed0e4ebe98f0323dc89cc0e074e1494aa4bc
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-04-10 22:42:32 +0330

    Update script.js

--- 09/script.js
+++ 09/script.js
@@ -52,7 +52,55 @@ const restaurant = {
 // Strings Part-3
 //////////////////////////////////////////////////
 
+// Split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('Amir Farhadi'.split(' '));
 
+const [firstName, lastName] = 'Amir Farhadi'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('amir farhadi');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Amir'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(2467624575427));
+console.log(maskCreditCard(7846764378234242));
+console.log(maskCreditCard('9878624675247687924'));
+
+// Repeat
+const message2 = 'Bad weather... All Departures Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'✈️'.repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(16);
 
 //////////////////////////////////////////////////
 // Strings Part-2
