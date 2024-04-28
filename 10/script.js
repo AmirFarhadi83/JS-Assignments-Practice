d94f731b16ceaae0e076e7e25dca11999c655e15
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-04-28 13:30:54 +0330

    Update script.js

--- 10/script.js
+++ 10/script.js
@@ -10,28 +10,28 @@
 // Default Parameters
 //////////////////////////////////////////////////
 
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers
-) {
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
-};
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
 
-createBooking('LH123');
-createBooking('LH123', 2, 800);
-createBooking('LH123', 2);
-createBooking('LH123', 5);
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
 
-createBooking('LH123', undefined, 1000);
+// createBooking('LH123', undefined, 1000);
 
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
