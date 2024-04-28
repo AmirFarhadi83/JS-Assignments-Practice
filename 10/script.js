28fdecd84f94373568a90737ff345b6a5b19e144
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-04-28 13:59:16 +0330

    Update script.js

--- 10/script.js
+++ 10/script.js
@@ -4,7 +4,36 @@
 // 004
 //////////////////////////////////////////////////
 
+const flight = 'LH234';
+const amir = {
+  name: 'Amir Farhadi',
+  passport: 123456789,
+};
 
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 123456789) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, amir);
+// console.log(flight);
+// console.log(amir);
+
+// // Is the same as doing...
+// const flightNum = flight;
+// const passenger = amir;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+newPassport(amir);
+checkIn(flight, amir);
 
 //////////////////////////////////////////////////
 // Default Parameters
