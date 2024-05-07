eefe60e02ace08e9c9ac41528bde984fcd63d534
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-05-07 11:36:43 +0330

    Update script.js

--- 10/script.js
+++ 10/script.js
@@ -4,22 +4,22 @@
 // 012
 //////////////////////////////////////////////////
 
-const secureBooking = function () {
-  let passengerCount = 0;
+// const secureBooking = function () {
+//   let passengerCount = 0;
 
-  return function () {
-    passengerCount++;
-    console.log(`${passengerCount} passengers`);
-  };
-};
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
 
-const booker = secureBooking();
+// const booker = secureBooking();
 
-booker();
-booker();
-booker();
+// booker();
+// booker();
+// booker();
 
-console.dir(booker);
+// console.dir(booker);
 
 //////////////////////////////////////////////////
 // 011
