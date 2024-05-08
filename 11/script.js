6be4bfee613a8f67ba64307914791e50c5f8f02f
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-05-08 14:16:17 +0330

    Update script.js

--- 11/script.js
+++ 11/script.js
@@ -81,6 +81,12 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance}€`;
+};
+calcDisplayBalance(account1.movements);
+
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
     acc.username = acc.owner
@@ -289,7 +295,8 @@ createUsernames(accounts);
 // 014
 /////////////////////////////////////////////////
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+console.log(movements);
 
 // // accumulator --> SNOWBALL
 // // const balance = movements.reduce(function (acc, cur, i, arr) {
@@ -302,3 +309,14 @@ createUsernames(accounts);
 // let balance2 = 0;
 // for (const mov of movements) balance2 += mov;
 // console.log(balance2);
+
+// Maximum value
+// const max = movements.reduce((acc, mov) => {
+//   if (acc > mov) return acc;
+//   else return mov;
+// }, movements[0]);
+const max = movements.reduce(
+  (acc, mov) => (acc > mov ? acc : mov),
+  movements[0]
+);
+console.log(max);
