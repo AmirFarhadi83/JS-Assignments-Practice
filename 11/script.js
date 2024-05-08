85fc2acc3b59be0c94476999e037b493e4129997
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-05-08 13:22:26 +0330

    Update script.js

--- 11/script.js
+++ 11/script.js
@@ -91,7 +91,6 @@ const createUsernames = function (accs) {
   });
 };
 createUsernames(accounts);
-console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -265,3 +264,23 @@ console.log(accounts);
 // );
 
 // console.log(movementDesc);
+
+/////////////////////////////////////////////////
+// 013
+/////////////////////////////////////////////////
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// const deposits = movements.filter(function (mov) {
+//   return mov > 0;
+// });
+const deposits = movements.filter(mov => mov > 0);
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
