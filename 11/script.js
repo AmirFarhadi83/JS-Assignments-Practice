153f9f734c65010725b7d30b7810c6afd321b4f1
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-05-08 13:22:38 +0330

    Update script.js

--- 11/script.js
+++ 11/script.js
@@ -269,18 +269,18 @@ createUsernames(accounts);
 // 013
 /////////////////////////////////////////////////
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-// const deposits = movements.filter(function (mov) {
-//   return mov > 0;
-// });
-const deposits = movements.filter(mov => mov > 0);
-console.log(movements);
-console.log(deposits);
+// // const deposits = movements.filter(function (mov) {
+// //   return mov > 0;
+// // });
+// const deposits = movements.filter(mov => mov > 0);
+// console.log(movements);
+// console.log(deposits);
 
-const depositsFor = [];
-for (const mov of movements) if (mov > 0) depositsFor.push(mov);
-console.log(depositsFor);
+// const depositsFor = [];
+// for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+// console.log(depositsFor);
 
-const withdrawals = movements.filter(mov => mov < 0);
-console.log(withdrawals);
+// const withdrawals = movements.filter(mov => mov < 0);
+// console.log(withdrawals);
