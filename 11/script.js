f350e24936dca53630fb152aabace99b5cb27e33
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-05-11 09:50:29 +0330

    Update script.js

--- 11/script.js
+++ 11/script.js
@@ -346,3 +346,23 @@ createUsernames(accounts);
 // calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 // console.log('----------------------------');
 // calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+/////////////////////////////////////////////////
+// 016
+/////////////////////////////////////////////////
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+console.log(movements);
+
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  // .map((mov, i, arr) => {
+  //   // console.log(arr);
+  //   return mov * eurToUsd;
+  // })
+  .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
