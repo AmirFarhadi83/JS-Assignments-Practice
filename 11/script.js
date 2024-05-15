6367df97abe9e333a816a1f860ab49a742f52dca
AmirFarhadi83 <frhadyamyr283@gmail.com>
2024-05-15 13:59:40 +0330

    Update script.js

--- 11/script.js
+++ 11/script.js
@@ -684,3 +684,7 @@ btnClose.addEventListener('click', function (e) {
 // console.log(convertTitleCase('this is a nice title'));
 // console.log(convertTitleCase('this is a LONG title but not too long'));
 // console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+
+/////////////////////////////////////////////////
+// Coding Challenge #4
+/////////////////////////////////////////////////
